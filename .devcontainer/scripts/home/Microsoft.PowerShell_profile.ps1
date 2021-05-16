649f66a604a8dd1410a76d4a7f3fd00578bd06f2
bathcat <eric.mcmullen@gmail.com>
2021-05-16 11:50:38 -0600

    0th commit-- placeholders.

---
+++ .devcontainer/scripts/home/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+if(-not (Get-Package 'oh-my-posh' -ErrorAction SilentlyContinue)){
+    Write-Host 'Installing oh-my-posh....'
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    Install-Module oh-my-posh -Scope CurrentUser -Force
+}
+
+Set-PoshPrompt -Theme pure

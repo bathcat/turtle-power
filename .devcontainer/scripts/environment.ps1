db18b3d5937e9bb683a3b942decbf3eb0a5b094f
bathcat <you@example.com>
2021-05-17 10:20:55 -0600

    Moving PoshPrompt installation to dockerfile.

---
+++ .devcontainer/scripts/environment.ps1
@@ -0,0 +1,3 @@
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module oh-my-posh -Scope AllUsers -Force
+Import-Module oh-my-posh
--- .devcontainer/scripts/profile.ps1
+++ .devcontainer/scripts/profile.ps1
@@ -1,12 +1,4 @@
-
-
-
-if(-not (Get-Package 'oh-my-posh' -ErrorAction SilentlyContinue)){
-    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-    Install-Module oh-my-posh -Scope CurrentUser -Force
-}
-
-Set-PoshPrompt -Theme pure
-
 # Opt out of telemetry
 $Env:POWERSHELL_TELEMETRY_OPTOUT = $true
+
+Set-PoshPrompt -Theme pure

4b898ae34ac15a0f62546f40d9bfc574849c0496
bathcat <you@example.com>
2021-05-27 10:09:14 -0600

    Adding tests.

--- labs/fizz-buzz/complete/Main.Tests.ps1
+++ labs/fizz-buzz/complete/Main.Tests.ps1
@@ -1,8 +1,21 @@
-Import-Module Pester -PassThru
 
-Describe 'Main' {
-    It 'Should think that true is true' {        
-        $true | Should -Be $true
+Describe 'Get-Message' {
+    
+    It 'Should be "11" for 11' {        
+        11 | Get-Message | Should -Be '11'
     }
+
+    It 'Should be "fizz" for 6' {        
+        6 | Get-Message | Should -Be 'fizz'
+    }
+
+    It 'Should be "buzz" for 10' {        
+        10 | Get-Message | Should -Be 'buzz'
+    }
+
+    It 'Should be "fizzbuzz" for 15' {        
+        15 | Get-Message | Should -Be 'fizzbuzz'
+    }
+
 }
 

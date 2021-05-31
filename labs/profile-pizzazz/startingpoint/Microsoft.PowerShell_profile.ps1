c7412eb5415bca799e7384511618f449b28b9e49
bathcat <you@example.com>
2021-05-31 13:18:56 -0700

    .

--- labs/profile-pizzazz/startingpoint/Microsoft.PowerShell_profile.ps1
+++ labs/profile-pizzazz/startingpoint/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
-Write-Host 'Hello world!'
+# To clobber your old profile, run this from a terminal:
+#     Get-Content Microsoft.PowerShell_profile.ps1 > $PROFILE
+
+Set-Item -Path Env:DOTNET_CLI_TELEMETRY_OPTOUT -Value 1 -Force
+Set-PoshPrompt -Theme ys

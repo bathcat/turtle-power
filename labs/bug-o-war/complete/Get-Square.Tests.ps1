32f7a306d40aab78e23948673c22291486cf28b7
bathcat <eric.mcmullen@gmail.com>
2021-06-08 16:33:22 -0600

    Tidying up.

--- labs/bug-o-war/complete/Get-Square.Tests.ps1
+++ labs/bug-o-war/complete/Get-Square.Tests.ps1
@@ -3,16 +3,16 @@ BeforeAll {
 }
 
 Describe 'Get-Square' {
-    
-    It 'Should work as a function' {        
+
+    It 'Should work as a function' {
         Get-Square 2 | Should -Be 4
     }
 
-    It 'Should work piped' {        
+    It 'Should work piped' {
         2 | Get-Square | Should -Be 4
     }
 
-    It 'Should work mapped' {        
+    It 'Should work mapped' {
         1,2,3 | Get-Square | Should -Be 1,4,9
     }
 

1656d18713a6d87c471ae92b8d144f01a1912360
bathcat <eric.mcmullen@gmail.com>
2021-06-11 11:47:16 -0600

    .

--- labs/tidy-documents/complete/Main.ps1
+++ labs/tidy-documents/complete/Main.ps1
@@ -1,8 +1,13 @@
+Set-StrictMode -Version Latest
+
+#region demo-setup
+New-Variable AssetsRoot "$(Split-Path $PSScriptRoot)/_assets" -Option Readonly, Private -Force 
+
+Import-Module -Force $AssetsRoot/Reset-Demo.ps1
+
+Reset-Demo $assetsRoot | Out-Null
+#endregion demo-setup
+
 Import-Module $PSScriptRoot/Optimize-Tidiness.ps1 -Force
+Optimize-Tidiness $assetsPath
 
-$path = $PSCommandPath |
-  Split-Path |
-  Split-Path |
-  Join-Path -ChildPath '_assets'
-
-Optimize-Tidiness $path
--- labs/tidy-documents/startingpoint/Main.ps1
+++ labs/tidy-documents/startingpoint/Main.ps1
@@ -1,11 +1,10 @@
-Set-StrictMode -Version Latest
+#region demo-setup
+New-Variable AssetsRoot "$(Split-Path $PSScriptRoot)/_assets" -Option Readonly, Private -Force 
+
+Import-Module -Force $AssetsRoot/Reset-Demo.ps1
+
+Reset-Demo $assetsRoot | Out-Null
+#endregion demo-setup
+
 Import-Module $PSScriptRoot/Optimize-Tidiness.ps1 -Force
-Import-Module $PSScriptRoot/Reset-Demo.ps1 -Force
-
-$target = $PSCommandPath |
-  Split-Path |
-  Split-Path |
-  Join-Path -ChildPath '_assets'
-
-Reset-Demo $target | Out-Null
-Optimize-Tidiness $target -WhatIf
+Optimize-Tidiness $assetsRoot

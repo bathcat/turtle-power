878b6943c2aa8dae2f7be1cd0ed8d66bca6f5129
bathcat <you@example.com>
2021-06-12 10:41:53 -0600

    Tidying up.

--- labs/log-analyzer/complete/Read-Entry.Tests.ps1
+++ labs/log-analyzer/complete/Read-Entry.Tests.ps1
@@ -6,9 +6,9 @@ BeforeAll {
 Describe 'Read-Entry' {
 
     
-    It 'Should get everything' {    
+    It 'Should get everything' {
         $entry = Read-Entry "02/12 11:01:06 INFO   :.....mailslot_create: creating mailslot for RSVP"
-        
+
         $entry.timestamp.Day | Should -Be 02
         $entry.severity | Should -Be "INFO"
         $entry.operation | Should -Be "mailslot_create"

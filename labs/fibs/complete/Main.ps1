2f334984803de910afab057f66fa0134917e345d
bathcat <you@example.com>
2021-06-16 09:49:44 -0600

    Tidying up.

--- labs/fibs/complete/Main.ps1
+++ labs/fibs/complete/Main.ps1
@@ -3,11 +3,9 @@ using module './Select-InRange.ps1'
 using module './Select-Evens.ps1'
 using module './Select-Sum.ps1'
 
-
 "Here is your answer: "
 
 Get-Fibs 60 |
-Select-InRange |
-Select-Evens |
-Select-Sum
-
+  Select-InRange |
+  Select-Evens |
+  Select-Sum

dcf4d883796cdae6e971cde88909d4c3ec0789b0
bathcat <you@example.com>
2021-06-16 10:21:58 -0600

    Commenting.

--- labs/fizz-buzz/startingpoint/Main.ps1
+++ labs/fizz-buzz/startingpoint/Main.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+  Gets the FizzBuzz message associated with a number.
+.INPUTS
+  Any integer.
+.OUTPUTS
+  The string message for the number.
+#>
 function Get-Message($i) {
   return [string]$i;
 }

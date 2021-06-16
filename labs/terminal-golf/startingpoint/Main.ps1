f517a8fe0ed0cfc2ed48116a5df1ddc3c026b012
bathcat <you@example.com>
2021-06-16 09:56:40 -0600

    Tidying up.

--- labs/terminal-golf/startingpoint/Main.ps1
+++ labs/terminal-golf/startingpoint/Main.ps1
@@ -1,1 +1,7 @@
-Write-Host 'Hello world!'
+Write-Host "`nHere are the goals:`n" -ForegroundColor Green
+Write-Host "================================================="
+
+Get-Content "$(Split-Path $PSScriptRoot)/README.MD"
+
+Write-Host "================================================="
+Write-Host "`n(You don't really need a script for this one.)`n" -ForegroundColor DarkGreen

0c9954c4ff02868a4995ed2ac1a77d991c40edb0
bathcat <you@example.com>
2021-07-02 07:42:57 -0700

    .

--- labs/restful-fortune/complete/Get-Kanye-Fortune.Tests.ps1
+++ labs/restful-fortune/complete/Get-Kanye-Fortune.Tests.ps1
@@ -1,15 +1,29 @@
 ﻿BeforeAll {
    Import-Module -Force $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+
 }
 Describe 'Get-Fortune' {
 
    It 'Should parse json properly' {
+      # Arrange
       [string]$expected = 'All the musicians will be free'
       Mock Invoke-RestMethod {
          return ConvertFrom-Json "{'quote':'$expected'}"
       }
 
-      Get-Fortune | Should -Be $expected
+      #Act
+      $actual = Get-Fortune
+
+      #Assert
+      $actual | Should -Be $expected
+   }
+
+   It 'Should throw on bogus result' {
+      Mock Invoke-RestMethod {
+         return ConvertFrom-Json '{"property":12}'
+      }
+
+      { Get-Fortune } | Should -Throw -ExceptionType ([System.Management.Automation.PropertyNotFoundException])
    }
 
 }

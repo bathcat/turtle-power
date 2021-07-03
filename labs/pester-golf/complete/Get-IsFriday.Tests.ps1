a2673e8fd1d3edc4aed723e4ba1fb90bab65b7f3
bathcat <eric.mcmullen@gmail.com>
2021-07-03 08:01:12 -0600

    Adding pester golf lab.

---
+++ labs/pester-golf/complete/Get-IsFriday.Tests.ps1
@@ -0,0 +1,27 @@
+﻿BeforeAll {
+   Import-Module -Force $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Get-IsFriday' {
+
+   It 'Should be true for for 2 July 2021' {
+
+      Mock Get-Date {
+         return [System.DateTime]::Parse('07-02-2021')
+      }
+
+      Get-IsFriday | Should -Be $true
+   }
+
+
+   It 'Should be false for for 1 July 2021' {
+
+      Mock Get-Date {
+         return [System.DateTime]::Parse('07-01-2021')
+      }
+
+      Get-IsFriday | Should -Be $false
+   }
+
+}
+
---
+++ labs/pester-golf/complete/Get-IsFriday.ps1
@@ -0,0 +1,14 @@
+﻿
+<#
+.SYNOPSIS
+   Returns a boolean indicating whether
+   it's Friday right now at this location.
+.OUTPUTS
+   $true means it's Friday, otherwise $false.
+#>
+function Get-IsFriday {
+   [CmdletBinding()]
+   [OutputType([bool])]
+   param()
+   return (Get-Date).DayOfWeek -eq 'Friday'
+}

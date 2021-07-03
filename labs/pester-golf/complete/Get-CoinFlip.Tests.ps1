9471b15d918a2cb3bdb116e81c24866ba4e29665
bathcat <eric.mcmullen@gmail.com>
2021-07-03 09:35:16 -0600

    Adding get-coinflip.

---
+++ labs/pester-golf/complete/Get-CoinFlip.Tests.ps1
@@ -0,0 +1,48 @@
+﻿BeforeAll {
+   Import-Module -Force $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe 'Get-CoinFlip' {
+
+
+
+   It 'Should be consistent with constant -Seed parameter' {
+      # Arrange
+      $samples = 100
+      $seed = Get-Random -Maximum 1.0 -Minimum 0.0
+
+      # Act
+      $first, $rest = 1..$samples |
+         % { Get-Coinflip -Seed $seed }
+
+      # Assert
+      $rest | % { $_ | Should -Be $first }
+   }
+
+   It 'Should be roughly 50% with default parameter' {
+      # Arrange
+      $samples = 500
+      $window = .2
+      $minimumHeads = $samples * .5 * (1 - $window)
+      $maximumHeads = $samples * .5 * (1 + $window)
+
+      # Act
+      $heads = 1..$samples |
+                  % { Get-Coinflip } |
+                  ? { $_ -eq [CoinFace]::Heads }
+
+      Write-Host "`n`nHeadCount: $($heads.Length)`n`n" -ForegroundColor Green
+
+      # Assert
+      $heads.Length |
+         Should -BeGreaterThan $minimumHeads |
+         Should -BeLessThan $maximumHeads
+   }
+
+
+
+}
+
+
+
+
---
+++ labs/pester-golf/complete/Get-CoinFlip.ps1
@@ -0,0 +1,31 @@
+﻿enum CoinFace {
+   Tails
+   Heads
+}
+
+<#
+.SYNOPSIS
+   Flips a virtual coin, returning [CoinFace]::Heads
+   or [CoinFace]::Tails with roughly equal frequency
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   PS C:\> <example usage>
+   Explanation of what the example does
+.INPUTS
+   Inputs (if any)
+.OUTPUTS
+   Output (if any)
+.NOTES
+   General notes
+#>
+function Get-CoinFlip {
+   [CmdletBinding()]
+   [OutputType([CoinFace])]
+   param(
+      [Parameter()]
+      [ValidateRange(0, 1)]
+      [float]$Seed = (Get-Random -Minimum 0.0 -Maximum 1.0)
+   )
+   return $Seed -gt .5 ? [CoinFace]::Heads : [CoinFace]::Tails
+}

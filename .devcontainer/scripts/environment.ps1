55ffdd96e3cf10e49ce3a1016384c70b7f4d0801
emoney <eric.mcmullen@gmail.com>
2023-01-25 09:14:35 -0300

    Getting rid of o-my-posh.

--- .devcontainer/scripts/environment.ps1
+++ .devcontainer/scripts/environment.ps1
@@ -1,6 +1,4 @@
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-Install-Module oh-my-posh -Scope AllUsers -Force
-Import-Module oh-my-posh
 
 Install-Module -Name Pester -Force -SkipPublisherCheck
 Import-Module Pester -PassThru
